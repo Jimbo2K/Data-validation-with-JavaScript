208108b3c510bcd38cd4032552d30a2507ab88b7
Pablo Garrido Roa <pablo.garrido.roa@gamil.com>
2016-07-23 23:36:49 +0200

    More CSS and JS for teh webpage. PGR

--- js/page.js
+++ js/page.js
@@ -1,12 +1,17 @@
 document.getElementById('btnlang').onclick=function(){
 	var btnn=document.getElementById('btnlang');
+	var htitle=document.getElementById('htitle');
 	if (btnn.innerHTML==='English') {
 		document.getElementById('eng-page').setAttribute('class', 'page-hidden');
 		document.getElementById('spn-page').setAttribute('class', 'page-show');
 		btnn.innerHTML='Spanish';
+		btnn.setAttribute('title', 'change to english');
+		htitle.innerHTML='Validación de datos con JavaScript';
 	} else {
 		document.getElementById('spn-page').setAttribute("class", "page-hidden");
 		document.getElementById('eng-page').setAttribute("class", "page-show");
 		btnn.innerHTML='English';
+		bttn.setAttribute('title', 'cambia a español');
+		htitle.innerHTML='Data validation with JavaScript';
 	}
 };
